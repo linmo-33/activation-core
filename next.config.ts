a1ed3866fcf067368dd8fed40e3b8fe60dda92b3
ljl <luo-111@qq.com>
2025-07-24 10:46:47 +0800

    修改配置

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,23 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
   eslint: {
-    // 在构建时禁用 ESLint
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // 如果也想禁用 TypeScript 检查
     ignoreBuildErrors: true,
   },
-  // 启用独立构建输出，用于Docker部署
   output: 'standalone',
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '@': path.resolve(__dirname, 'src'),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
