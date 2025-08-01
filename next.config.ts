8f2bef5ddc39093bedd9d6b5365c5ddc7b16fd37
linmo <68427512+linmo-33@users.noreply.github.com>
2025-08-01 20:46:24 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // 警告：这将在生产构建期间完全禁用 ESLint。
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
